8ccf847889295e2650f95847968f6a4bc6e4cbeb
Daniel <contato.daniel.chiudini@gmail.com>
2026-08-04 19:38:29 -0300

    feat: dialog shadcn e plugin process

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -50,6 +50,7 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(external_navigation_plugin())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
